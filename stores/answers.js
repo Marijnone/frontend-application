ab2c7fa49374d48f830928878fa192627d2bd097
Marijnone <marijn.moviat@hva.nl>
2018-10-18 13:15:11 +0200

    Sum is working

--- stores/answers.js
+++ stores/answers.js
@@ -1,42 +1,47 @@
-
 module.exports = answers
 
 
-function answers (state, emitter) {
+function answers(state, emitter) {
   state.answers = []
 
-    emitter.on('answers:add', function (answers) {
-      console.log("gaat om deze", answers);
-      
-            var {type, value, gewicht} = answers; //to define the type and value Object.assign(waar het in moet, waar je het uithaalt)
-            var obj = {type: type, value: value}
-            
-            state.answers.push(obj)
- 
-            state.percentage = formula(answers, emitter)
+  emitter.on('answers:add', function (answers) {
+    console.log("gaat om deze", answers);
 
-            // console.log(answers);
-            // console.log(value);
-            // console.log(gewicht);
-            // console.log(state.answers);
-            
-        
-            emitter.emit('render')
-        
-        function formula (answers, emitter) {
-          var gewichten = []
-          console.log(answers);
-          
-          answers.forEach(function(answer){
-            // console.log(answer);
-            
-          })
-          };
-        
-        emitter.emit(state.events.RENDER)
-    })
+    var {
+      type,
+      value,
+      gewicht
+    } = answers; //to define the type and value Object.assign(waar het in moet, waar je het uithaalt)
+    var obj = {
+      type: type,
+      value: value
+    }
+
+    state.answers.push(obj)
+
+    state.percentage = formula(answers, emitter)
+
+
+    emitter.emit('render')
+
+    function formula(answers, emitter) {
+      var gewichten = []
+      answers.forEach(function (answer) {
+        var gewicht = answer.gewicht
+        gewichten.push(gewicht)
+      })
+      var sum = gewichten.reduce(function (accumulator, currentValue) {
+        return Number(accumulator) + Number(currentValue)
+      }, 0)
+      var percentage = Number((1 / (1 + Math.exp(-1 * (-8.57219 + sum))) * 100).toFixed(2))
+
+      console.log(percentage);
+      return percentage
+    };
+
+    emitter.emit(state.events.RENDER)
+  })
 
 
 
 }
-
